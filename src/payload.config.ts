1630195566c15695696722ae40e18a76316415bc
taiwo2 <adetonat@ymail.com>
2024-06-03 09:13:17 +0100

    Fix CMS payload setup

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,12 +3,12 @@ import { webpackBundler } from '@payloadcms/bundler-webpack'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { slateEditor } from '@payloadcms/richtext-slate'
 import path from 'path'
-import { Users } from './collections/Users'
+// import { Users } from './collections/Users'
 import dotenv from 'dotenv'
-import { Products } from './collections/Products/Products'
-import { Media } from './collections/Media'
-import { ProductFiles } from './collections/ProductFile'
-import { Orders } from './collections/Orders'
+// import { Products } from './collections/Products/Products'
+// import { Media } from './collections/Media'
+// import { ProductFiles } from './collections/ProductFile'
+// import { Orders } from './collections/Orders'
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
@@ -16,15 +16,16 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  collections: [Users, Products, Media, ProductFiles, Orders], 
+  collections: [],
+  // [Users, Products, Media, ProductFiles, Orders], 
   routes: {
     admin: '/sell',
   },
   admin: {
-    user: 'users',
+    // user: 'users',
     bundler: webpackBundler(),
     meta: {
-      titleSuffix: '- DigitalHippo',
+      titleSuffix: 'Market Place',
       favicon: '/favicon.ico',
       ogImage: '/thumbnail.jpg',
     },
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express'
 import { getPayloadClient } from './get-payload'
 import { nextApp, nextHandler } from './next-utils'
-import * as trpcExpress from '@trpc/server/adapters/express'
-import { appRouter } from './trpc'
-import { inferAsyncReturnType } from '@trpc/server'
+// import * as trpcExpress from '@trpc/server/adapters/express'
+// import { appRouter } from './trpc'
+// import { inferAsyncReturnType } from '@trpc/server'
 import bodyParser from 'body-parser'
 import { IncomingMessage } from 'http'
-import { stripeWebhookHandler } from './webhooks'
+// import { stripeWebhookHandler } from './webhooks'
 import nextBuild from 'next/dist/build'
 import path from 'path'
 import { PayloadRequest } from 'payload/types'
@@ -15,34 +15,34 @@ import { parse } from 'url'
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
 
-const createContext = ({
-  req,
-  res,
-}: trpcExpress.CreateExpressContextOptions) => ({
-  req,
-  res,
-})
+// const createContext = ({
+//   req,
+//   res,
+// }: trpcExpress.CreateExpressContextOptions) => ({
+//   req,
+//   res,
+// })
 
-export type ExpressContext = inferAsyncReturnType<
-  typeof createContext
->
+// export type ExpressContext = inferAsyncReturnType<
+//   typeof createContext
+// >
 
-export type WebhookRequest = IncomingMessage & {
-  rawBody: Buffer
-}
+// export type WebhookRequest = IncomingMessage & {
+//   rawBody: Buffer
+// }
 
 const start = async () => {
-  const webhookMiddleware = bodyParser.json({
-    verify: (req: WebhookRequest, _, buffer) => {
-      req.rawBody = buffer
-    },
-  })
+  // const webhookMiddleware = bodyParser.json({
+  //   verify: (req: WebhookRequest, _, buffer) => {
+  //     req.rawBody = buffer
+  //   },
+  // })
 
-  app.post(
-    '/api/webhooks/stripe',
-    webhookMiddleware,
-    stripeWebhookHandler
-  )
+  // app.post(
+  //   '/api/webhooks/stripe',
+  //   webhookMiddleware,
+  //   stripeWebhookHandler
+  // )
 
   const payload = await getPayloadClient({
     initOptions: {
@@ -53,45 +53,45 @@ const start = async () => {
     },
   })
 
-  if (process.env.NEXT_BUILD) {
-    app.listen(PORT, async () => {
-      payload.logger.info(
-        'Next.js is building for production'
-      )
+  // if (process.env.NEXT_BUILD) {
+  //   app.listen(PORT, async () => {
+  //     payload.logger.info(
+  //       'Next.js is building for production'
+  //     )
 
-      // @ts-expect-error
-      await nextBuild(path.join(__dirname, '../'))
+  //     // @ts-expect-error
+  //     await nextBuild(path.join(__dirname, '../'))
 
-      process.exit()
-    })
+  //     process.exit()
+  //   })
 
-    return
-  }
+  //   return
+  // }
 
-  const cartRouter = express.Router()
+  // const cartRouter = express.Router()
 
-  cartRouter.use(payload.authenticate)
+  // cartRouter.use(payload.authenticate)
 
-  cartRouter.get('/', (req, res) => {
-    const request = req as PayloadRequest
+  // cartRouter.get('/', (req, res) => {
+  //   const request = req as PayloadRequest
 
-    if (!request.user)
-      return res.redirect('/sign-in?origin=cart')
+  //   if (!request.user)
+  //     return res.redirect('/sign-in?origin=cart')
 
-    const parsedUrl = parse(req.url, true)
-    const { query } = parsedUrl
+  //   const parsedUrl = parse(req.url, true)
+  //   const { query } = parsedUrl
 
-    return nextApp.render(req, res, '/cart', query)
-  })
+  //   return nextApp.render(req, res, '/cart', query)
+  // })
 
-  app.use('/cart', cartRouter)
-  app.use(
-    '/api/trpc',
-    trpcExpress.createExpressMiddleware({
-      router: appRouter,
-      createContext,
-    })
-  )
+  // app.use('/cart', cartRouter)
+  // app.use(
+  //   '/api/trpc',
+  //   trpcExpress.createExpressMiddleware({
+  //     router: appRouter,
+  //     createContext,
+  //   })
+  // )
 
   app.use((req, res) => nextHandler(req, res))
 
