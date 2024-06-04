b3bd582451f59ab0e226a4a5401717d893470e58
taiwo2 <adetonat@ymail.com>
2024-06-04 10:20:06 +0100

    Add signup page

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { Icons } from '@/components/Icons'
+import {
+  Button,
+  buttonVariants,
+} from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
+import Link from 'next/link'
+
+
+const Page = () => {
+  
+  return (
+    <>
+      <div className='container relative flex pt-20 flex-col items-center justify-center lg:px-0'>
+        <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
+          <div className='flex flex-col items-center space-y-2 text-center'>
+            <Icons.logo className='h-20 w-20' />
+            <h1 className='text-2xl font-semibold tracking-tight'>
+              Create an account
+            </h1>
+
+            <Link
+              className={buttonVariants({
+                variant: 'link',
+                className: 'gap-1.5',
+              })}
+              href='/sign-in'>
+              Already have an account? Sign-in
+              <ArrowRight className='h-4 w-4' />
+            </Link>
+          </div>
+
+          <div className='grid gap-6'>
+       
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Page
